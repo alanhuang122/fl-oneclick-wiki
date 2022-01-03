dd8e50a26413a60cad7a6fb86d688aee0a72a763
Konstantin Kotenko <konstantin.kotenko@rwth-aachen.de>
2022-01-03 14:45:21 +0100

    use game type:Quality instead of categories

--- inject.js
+++ inject.js
@@ -173,8 +173,7 @@
                         }
 
                         if (myself){
-                            categories = ["Pyramidal Qualities", "Discrete Qualities"];
-                            //TODO: "Qualities" might also work
+                            categories = ["Quality"];
                         }
                         const wikiButton = createWikiButton(myself);
                         wikiButton.addEventListener("click", () => {
